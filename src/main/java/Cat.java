2993c9120771849ebfb589d56995de5ecfff3244
Ruslan Tulumbaev <rtulumbaev@gmail.com>
2023-01-05 16:29:33 +0500

    add Cat Bean

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String catName;
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+}
